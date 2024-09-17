5f1baf547d68396790ea3cc314d37d71ef4d2b4c
Andre Flego <ade.flego@gmail.com>
2024-09-17 23:54:57 +0200

    update ProtectedRoutes loading state

--- src/components/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet, useLocation } from 'react-router';
 import { useAuth } from '../../providers/AuthProvider';
 import { Loading } from '../Loading';
+import styles from './ProtectedRoutes.module.css';
 
 export const ProtectedRoutes = () => {
   const auth = useAuth();
@@ -8,7 +9,11 @@ export const ProtectedRoutes = () => {
   const location = useLocation();
 
   if (auth.isLoading) {
-    return <Loading />;
+    return (
+      <div className={styles.spinnerContainer}>
+        <Loading />
+      </div>
+    );
   }
 
   return isAuthenticated ? <Outlet /> : <Navigate to={'/auth'} state={{ from: { location } }} />;
