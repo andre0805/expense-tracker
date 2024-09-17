4bf00a1b61672baa71b9e6b0896f6abc0d0cec49
Andre Flego <ade.flego@gmail.com>
2024-09-17 23:54:18 +0200

    update Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,9 +2,5 @@ import styles from './Loading.module.css';
 
 export const Loading = () => {
   // create circle loading
-  return (
-    <div className={styles.spinnerContainer}>
-      <div className={styles.spinner}></div>
-    </div>
-  );
+  return <div className={styles.spinner}></div>;
 };
