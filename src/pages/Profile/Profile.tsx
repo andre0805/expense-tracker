a2a2e22ddf9c4d2429f401e1c7344bb66a8c90d1
Andre Flego <ade.flego@gmail.com>
2024-09-17 15:39:07 +0200

    implement Profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,17 +1,33 @@
 import { useAuth } from '../../providers/AuthProvider';
 import { Button } from '@mantine/core';
+import styles from './Profile.module.css';
+import avatarPlaceholder from '../../assets/avatar.svg';
+import { IconLogout } from '@tabler/icons-react';
 
 export const Profile = () => {
   const auth = useAuth();
+  const user = auth.user;
 
   const logout = () => {
     return auth.logout();
   };
 
+  const profileImage = user?.photoURL ? user.photoURL : avatarPlaceholder;
+
   return (
-    <div>
-      <p>Logged in as {auth.user?.email}</p>
-      <Button onClick={logout}>Logout</Button>
+    <div className={styles.container}>
+      <div className={styles.userDetails}>
+        <img src={profileImage} alt="User profile" className={styles.profileImage} />
+        <p className={styles.name}>{user?.displayName}</p>
+        <p className={styles.email}>{user?.email}</p>
+      </div>
+      <Button
+        className={styles.logoutButton}
+        leftSection={<IconLogout size={16} />}
+        onClick={logout}
+      >
+        Logout
+      </Button>
     </div>
   );
 };
