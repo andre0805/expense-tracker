3d46f8dbb87057e81f630d7cf9a5c618fbd6156f
Andre Flego <ade.flego@gmail.com>
2024-09-18 11:54:53 +0200

    Update Transaction.ts

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -6,12 +6,19 @@ export class Transaction {
   amount: number;
   description: string;
   date: Date;
+  dateString: string;
 
-  constructor(description: string, amount: number, category: Category) {
+  constructor(description: string, amount: number, category: Category, date?: Date) {
     this.id = Math.random().toString(36);
     this.description = description;
     this.amount = amount;
-    this.date = new Date();
+    this.date = date ?? new Date();
     this.category = category;
+
+    // Format date as Mar 23
+    this.dateString = this.date.toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+    });
   }
 }
