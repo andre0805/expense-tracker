5d18a9b9b6dd7096952bce46cb2a00e61d413388
Andre Flego <ade.flego@gmail.com>
2024-09-19 23:49:29 +0200

    refactor NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 import { Logo } from '../Logo';
+import { Flex, Text } from '@mantine/core';
 
 export const NavBar = () => {
   return (
     <nav>
-      <Logo />
+      <Flex justify={'center'} align={'center'} gap={12}>
+        <Logo />
+
+        <NavLink to={'/'}>
+          <Text size={'1.1rem'} fw={700} c={'var(--mantine-color-white)'}>
+            Expense
+            <br />
+            Tracker
+          </Text>
+        </NavLink>
+      </Flex>
 
       <NavLink to={'/profile'} className={styles.link}>
         Profile
